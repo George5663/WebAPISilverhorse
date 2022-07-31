d909f557b2a31e52a34e16736a0db8e12515264f
George4788 <20221577@student.curtin.edu.au>
2022-08-01 00:32:01 +0800

    Implemented HTTP methods for datasource

---
+++ WebAPISilverhorse/Database/DataIn.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace WebAPISilverhorse.Database
+{
+    public class DataIn
+    {
+        //Using RESTful HTTP Methods to recieve the data from the datasource, good for scalability
+        private static HttpClient httpClient = new HttpClient();
+        private static string defaultUrl = "https://jsonplaceholder.typicode.com/";
+        private static HttpResponseMessage response;
+
+        //Used to recieve the list of data from the datasource
+        public static async Task<string> Get(string dataName)
+        {
+            string url = defaultUrl + dataName;
+
+            response = await httpClient.GetAsync(url);
+
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        //Used to create link
+        public static async Task<string> Post(string dataName, object objectIn)
+        {
+            //Converting the Json Object
+            HttpContent httpContent = new StringContent(JsonConvert.SerializeObject(objectIn), Encoding.UTF8);
+            response = await httpClient.PostAsync(defaultUrl + dataName, httpContent);
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        //Used to update a specific id in the list of data
+        public static async Task<string> Put(string dataName, object objectIn, int id)
+        {
+            HttpContent httpContent = new StringContent(JsonConvert.SerializeObject(objectIn), Encoding.UTF8);
+            response = await httpClient.PostAsync(defaultUrl + dataName + "/" + id.ToString(), httpContent);
+
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        //Used to delete a resource within the list of data
+        public static async Task<string> Delete(string dataName, int id)
+        {
+            response = await httpClient.DeleteAsync(defaultUrl + dataName + "/" + id.ToString());
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
